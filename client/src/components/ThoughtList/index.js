b2accb50447e1e910be071fc3835a6f9ffd1be59
Jake P <76881086+jbped@users.noreply.github.com>
2021-09-07 17:42:36 -0600

    added routes for pages

--- client/src/components/ThoughtList/index.js
+++ client/src/components/ThoughtList/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
 
-export default function ThoughtList ({ thoughts, title }) {
-    if(!thoughts.length) {
+export default function ThoughtList({ thoughts, title }) {
+    if (!thoughts.length) {
         return <h3>No thoughts yet</h3>
     }
     return (
@@ -12,14 +13,23 @@ export default function ThoughtList ({ thoughts, title }) {
             {thoughts && thoughts.map(thought => (
                 <div key={thought._id} className="card mb-3">
                     <p className="card-header">
-                        {thought.username} thought on {thought.createdAt}
+                        <Link
+                            to={`/profile/${thought.username}`}
+                            style={{ fontWeight: 700 }}
+                            className="text-light"
+                        >
+                            {thought.username}
+                        </Link>{' '}
+                        thought on {thought.createdAt}
                     </p>
                     <div className="card-body">
-                        <p>{thought.thoughtText}</p>
-                        <p className="mb-0">
-                            Reactions: {thought.reactionCount} || Click to {' '}
-                            {thought.reactionCount ? 'see' : 'start'} the discussion!
-                        </p>
+                        <Link to={`/thought/${thought._id}`}>
+                            <p>{thought.thoughtText}</p>
+                            <p className="mb-0">
+                                Reactions: {thought.reactionCount} || Click to {' '}
+                                {thought.reactionCount ? 'see' : 'start'} the discussion!
+                            </p>
+                        </Link>
                     </div>
                 </div>
             ))}
